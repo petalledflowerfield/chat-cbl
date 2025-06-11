52bb196a0e5739a1ed7ed57a7533dfa155916e66
Phoebe Dusil <l.c.dusil@student.tue.nl>
2025-06-11 21:19:53 +0200

    Add script for simplifying everything

---
+++ run.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+export TURTLEBOT3_MODEL=burger
+
+read -p "IP " bot_ip
+
+ssh ubuntu@$bot_ip << EOF
+export TURTLEBOT3_MODEL=$TURTLEBOT3_MODEL
+ros2 launch turtlebot3_bringup robot.launch.py &
+ros2 run usb_cam usb_cam_node_exe &
+exit
+EOF
+
+mkdir -p ~/turtlebot3_ws/src
+
+git clone https://github.com/petalledflowerfield/turtlebot3_simulations -b foxy-devel ~/turtlebot3_ws/src/turtlebot3_simulations
+
+cp -r crowd_nav2 fake_node phyvir signaling smart_explorer trash_map yolo_detector ~/turtlebot3_ws/src
+
+cd ~/turtlebot3_ws
+
+colcon build
+source install/setup.bash
+
+read -p "Press enter to launch virtual environment"
+
+ros2 launch turtlebot3_gazebo first_world.launch.py
+
+read -p "Place the robot in the same location as in the virtual environment"
+read -p "Run teleop in a separate window and move the robot around to check the scale of the virtual environment with the physical environment (if adjustments needed, exit with Ctrl-C)"
+read -p "Press enter to run necessary nodes"
+
+ros2 run phyvir phyvir_passthru
+ros2 run smart_explorer smart_explorer
+ros2 run yolo_detector yolo_detector
